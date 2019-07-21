c82e4f5c447e9bb45bbe9b54e0f7365df1215864
Forb <nikolay.mashkov@gmail.com>
2019-07-21 14:07:04 +0300

    добавление рандомизации

--- fixture/group.py
+++ fixture/group.py
@@ -41,23 +41,32 @@ class GroupHelper:
             driver.find_element_by_name(field_name).send_keys(text)
 
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self,index):
         driver = self.app.driver
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         #submit deletion
         driver.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
 
+    def select_group_by_index(self,index):
+        driver = self.app.driver
+        driver.find_elements_by_name("selected[]")[index].click()
+
     def select_first_group(self):
         driver = self.app.driver
         driver.find_element_by_name("selected[]").click()
 
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
 
-    def modify_first_group(self,new_group_data):
+    def modify_group_by_index(self,index,new_group_data):
         driver = self.app.driver
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         #open modification form
         driver.find_element_by_name("edit").click()
         #fill group form
