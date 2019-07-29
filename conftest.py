fc204514f10a403832fec87f94a3eb834ab700ef
Forb <nikolay.mashkov@gmail.com>
2019-07-30 01:27:32 +0300

    добавил разные браузеры для запуска с командной строки

--- conftest.py
+++ conftest.py
@@ -8,11 +8,13 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture=Application()
+        fixture=Application(browser=browser,base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser,base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -25,3 +27,7 @@ def stop(request):
     request.addfinalizer(fin)
 
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser",action="store",default="chrome")
+    parser.addoption("--baseUrl",action="store",default="http://localhost/addressbook")
--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium import webdriver
 
 from fixture.group import GroupHelper
 from fixture.session import SessionHelper
@@ -7,12 +7,21 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.driver=WebDriver()
+    def __init__(self,browser,base_url):
+        if browser=="firefox":
+            self.driver=webdriver.Firefox()
+        elif browser=="chrome":
+            self.driver=webdriver.Chrome()
+        elif browser == "ie":
+            self.driver = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized broser %s" % browser)
+        self.driver=webdriver.Chrome()
         self.driver.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact=ContactHelper(self)
+        self.base_url=base_url
 
     def is_valid(self):
         try:
@@ -23,7 +32,7 @@ class Application:
 
     def open_home_page(self):
         driver=self.driver
-        driver.get("http://localhost:8080/addressbook")
+        driver.get(self.base_url)
 
     def destroy(self):
         self.driver.quit()
