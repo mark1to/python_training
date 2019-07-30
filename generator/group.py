e5296571f4f50f3fba9cd77e3648fd1bd4cf912e
Forb <nikolay.mashkov@gmail.com>
2019-07-30 17:17:06 +0300

    загрузка из json

--- generator/group.py
+++ generator/group.py
@@ -1,4 +1,4 @@
-import json
+import jsonpickle
 from model.group import Group
 import random
 import string
@@ -37,4 +37,5 @@ testdata=[ Group(name="", header="", footer="")],[
 file=os.path.join(os.path.dirname(os.path.abspath(__file__)),"..",f)
 
 with open(file,"w") as out:
-    out.write(json.dumps(testdata,default=lambda x:x.__dict__,indent=2))
+    jsonpickle.set_encoder_options("json",indent=2)
+    out.write(jsonpickle.encode(testdata))
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,18 +1,28 @@
 from sys import maxsize
 from model.group import Group
 import pytest
-from data.add_group import constant as testdata
 
 
-
-@pytest.mark.parametrize("group",testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app,group):
+def test_add_group(app,json_groups):
+    group=json_groups
     old_groups=app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) +1 ==app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups,key=Group.id_or_max)==sorted(new_groups,key=Group.id_or_max)
+
+
+
+
+# def test_add_group(app,data_groups):
+#     group=data_groups
+#     old_groups=app.group.get_group_list()
+#     app.group.create(group)
+#     assert len(old_groups) +1 ==app.group.count()
+#     new_groups = app.group.get_group_list()
+#     old_groups.append(group)
+#     assert sorted(old_groups,key=Group.id_or_max)==sorted(new_groups,key=Group.id_or_max)
 
 
 
